30492f1f18aaf2792d72a4c2c3b364cf0a9ef17a
rodi <mtarslan96@gmail.com>
2018-12-24 02:06:18 +0300

    handle bugs

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from accounts.views import *
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('register/buyer', register_doctor, name= 'register_doctor'),
-    path('register/seller', register_patient, name= 'register_patient'),
+    path('register/doctor', register_doctor, name= 'register_doctor'),
+    path('register/patient', register_patient, name= 'register_patient'),
     path('logout/', logout, name= 'logout'),
     path('profile/', profile, name= 'profile'),
     path('', get_home, name = 'home'),
