9d6b09a5113c9a3fc82701ebcd315da84aebd46f
rodi <mtarslan96@gmail.com>
2018-12-24 00:47:59 +0300

    change the user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Doctor, Patient
+from .models import Doctor, Patient, MedUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 
 admin.site.register(Doctor)
 admin.site.register(Patient)
+admin.site.register(MedUser,UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
 
-class MedUser(User):
+class MedUser(AbstractUser):
   USER_TYPE_CHOICES = (
       (1, 'doctor'),
       (2, 'patient'),
@@ -11,11 +12,11 @@ class MedUser(User):
       (4, 'relative')
   )
 
-  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+  user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES, null=True)
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField(MedUser, on_delete=models.CASCADE, related_name='doctor')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='doctor')
     verified = models.BooleanField(default=False)
 
     def __str__(self):
@@ -23,7 +24,7 @@ class Doctor(models.Model):
 
 
 class Patient(models.Model):
-    user = models.OneToOneField(MedUser, on_delete=models.CASCADE, related_name='patient')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='patient')
     birth_date = models.BooleanField(blank=True)
 
     def __str__(self):
