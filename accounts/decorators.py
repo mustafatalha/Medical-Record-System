e4d95d5f6d739da4150e709d559117178f692aea
rodi <mtarslan96@gmail.com>
2018-12-24 20:05:01 +0300

    add patient_login_required decorator

--- accounts/decorators.py
+++ accounts/decorators.py
@@ -3,7 +3,14 @@ from django.contrib.auth.decorators import login_required
 
 
 def doctor_login_required(view_func):
-    rec_login_required = user_passes_test(lambda u: u.is_active and (u.user_type == 1),
+    doc_login_required = user_passes_test(lambda u: u.is_active and (u.user_type == 1),
                                           login_url='/accounts/login')
-    decorated_view_func = login_required(rec_login_required(view_func), login_url='/accounts/login')
+    decorated_view_func = login_required(doc_login_required(view_func), login_url='/accounts/login')
+    return decorated_view_func
+
+
+def patient_login_required(view_func):
+    pat_login_required = user_passes_test(lambda u: u.is_active and (u.user_type == 2),
+                                          login_url='/accounts/login')
+    decorated_view_func = login_required(pat_login_required(view_func), login_url='/accounts/login')
     return decorated_view_func
